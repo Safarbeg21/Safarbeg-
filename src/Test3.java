0c50b2d5b1c9bb2db79dc5dcbfa9897427117291
Safarbeg <safarbeg4242@gmail.com>
2021-06-16 19:06:59 -0500

    safarii

--- src/Test3.java
+++ src/Test3.java
@@ -1,6 +1,7 @@
 public class Test3 {
     public static void main(String[] args) {
-        System.out.println("My  practice 2 ");
+        String name="Sheri nar";
+        System.out.println(name+"My  practice 2 ");
         System.out.println("my name is safarbeg");
     }
 }
