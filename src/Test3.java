53e9121bec02c44358db3dc59568ac57dd03707c
Safarbeg <safarbeg4242@gmail.com>
2021-06-17 18:36:02 -0500

    new project

--- src/Test3.java
+++ src/Test3.java
@@ -1,7 +1,7 @@
 public class Test3 {
     public static void main(String[] args) {
         String name="Sheri nar";
-        System.out.println(name+"My  practice 2 ");
+        System.out.println(name+" My  practice 2 ");
         System.out.println("my name is safarbeg");
     }
 }
---
+++ src/myProjectTest1.java
@@ -0,0 +1,6 @@
+public class myProjectTest1 {
+    public static void main(String[] args) {
+        String name2="Safarbeg";
+        System.out.println(name2+"my new project ");
+    }
+}
